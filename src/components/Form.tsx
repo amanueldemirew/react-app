90f144d44365eb791482f3778862f09f7e5fdeb5
amanueldemirew <amanuel.demirew@yahoo.com>
2024-09-16 16:20:43 +0300

    "Added react-hook-form and zod dependencies, updated Form component to use react-hook-form, and modified package.json and package-lock.json accordingly."

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,25 +1,58 @@
-import React from "react";
+import React, { FormEvent, useRef, useState } from "react";
+import { FieldValue, FieldValues, useForm, FormState } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  console.log(register("name"));
+
+  const onSubmit = (data: FieldValues) => console.log(data);
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        console.log("submitted");
-      }}
-    >
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be at least 3 character.</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          {...register("age", { required: true, minLength: 1, maxLength: 3 })}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
+      {errors.age?.type === "required" && (
+        <p className="text-danger">The age field is required.</p>
+      )}
+      {errors.age?.type === "maxLength" && (
+        <p className="text-danger">The age must be at most 3 character.</p>
+      )}
       <button className="btn btn-primary">submit</button>
     </form>
   );
